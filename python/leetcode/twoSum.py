d598fd299b46e69beb4e0ef6c94f5585144f1ece
Subanu, Mahamadrafi <mahamadrafi.subanu@ruckusnetworks.com>
2026-07-20 18:16:01 +0530

    Fixed a typo

--- python/leetcode/twoSum.py
+++ python/leetcode/twoSum.py
@@ -10,7 +10,7 @@ from typing import List
 
 class Solution:
 
-    def twoSum(self, nums: List[int], target[int]) -> List[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen : dict[int, int] = {}
         for i,num in enumerate(nums):
             complement = target - num
